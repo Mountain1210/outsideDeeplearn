5fb9b5031c6dc3af128d9ec87712ba831bb388f2
Mountain1210 <works07@126.com>
2018-05-27 15:07:41 +0800

    深度学习

---
+++ README.md
@@ -0,0 +1,16 @@
+# Neural Network in JavaScript with Deeplearn.js
+
+[![Build Status](https://travis-ci.org/javascript-machine-learning/mnist-neural-network-deeplearnjs.svg?branch=master)](https://travis-ci.org/javascript-machine-learning/mnist-neural-network-deeplearnjs)
+
+Use Case: Learning best color matches of font and background color for an improved web accessibility.
+
+This example project demonstrates how neural networks may be used to solve a binary classification problem. It uses [deeplearn.js](https://deeplearnjs.org/) to predict accessible font colors based on background colors. [Read more about it](https://www.robinwieruch.de/neural-networks-deeplearnjs-javascript). If you have problems to follow the view layer implementation with React, checkout [this book](https://roadtoreact.com/) to learn the fundamentals of it.
+
+![nov-26-2017 16-15-23](https://user-images.githubusercontent.com/2479967/33238261-0c4306f6-d2c5-11e7-9ce8-4e661a307b85.gif)
+
+## Installation
+
+* `git clone git@github.com:javascript-machine-learning/color-accessibility-neural-network-deeplearnjs.git`
+* `cd color-accessibility-neural-network-deeplearnjs`
+* `npm install`
+* `npm start`
---
+++ src/App.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+import generateColorSet from './data';
+import ColorAccessibilityModel from './neuralNetwork';
+
+const ITERATIONS = 750;
+const TRAINING_SET_SIZE = 1500;
+const TEST_SET_SIZE = 10;
+
+class App extends Component {
+
+  testSet;
+  trainingSet;
+  colorAccessibilityModel;
+
+  constructor() {
+    super();
+
+    this.testSet = generateColorSet(TEST_SET_SIZE);
+    this.trainingSet = generateColorSet(TRAINING_SET_SIZE);
+
+    this.colorAccessibilityModel = new ColorAccessibilityModel();
+    this.colorAccessibilityModel.setupSession(this.trainingSet);
+
+    this.state = {
+      currentIteration: 0,
+      cost: -1,
+    };
+  }
+
+  componentDidMount () {
+    requestAnimationFrame(this.tick);
+  };
+
+  tick = () => {
+    this.setState((state) => ({ currentIteration: state.currentIteration + 1 }));
+
+    if (this.state.currentIteration < ITERATIONS) {
+      requestAnimationFrame(this.tick);
+
+      let computeCost = !(this.state.currentIteration % 5);
+      let cost = this.colorAccessibilityModel.train(this.state.currentIteration, computeCost);
+
+      if (cost > 0) {
+        this.setState(() => ({ cost }));
+      }
+    }
+  };
+
+  render() {
+    const { currentIteration, cost } = this.state;
+
+    return (
+      <div className="app">
+        <div>
+          <h1>Neural Network for Web Accessibility in JavaScript</h1>
+          <p>Iterations: {currentIteration}</p>
+          <p>Cost: {cost}</p>
+        </div>
+
+        <div className="content">
+          <div className="content-item">
+            <ActualTable
+              testSet={this.testSet}
+            />
+          </div>
+
+          <div className="content-item">
+            <InferenceTable
+              model={this.colorAccessibilityModel}
+              testSet={this.testSet}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const ActualTable = ({ testSet }) =>
+  <div>
+    <p>Programmatically Computed</p>
+
+    {Array(TEST_SET_SIZE).fill(0).map((v, i) =>
+      <ColorBox
+        key={i}
+        rgbInput={testSet.rawInputs[i]}
+        rgbTarget={fromClassifierToRgb(testSet.rawTargets[i])}
+      />
+    )}
+  </div>
+
+const InferenceTable = ({ testSet, model }) =>
+  <div>
+    <p>Neural Network Computed</p>
+    {Array(TEST_SET_SIZE).fill(0).map((v, i) =>
+      <ColorBox
+        key={i}
+        rgbInput={testSet.rawInputs[i]}
+        rgbTarget={fromClassifierToRgb(model.predict(testSet.rawInputs[i]))}
+      />
+    )}
+  </div>
+
+const ColorBox = ({ rgbInput, rgbTarget }) =>
+  <div className="color-box" style={{ backgroundColor: getRgbStyle(rgbInput) }}>
+    <span style={{ color: getRgbStyle(rgbTarget) }}>
+      <RgbString rgb={rgbInput} />
+    </span>
+  </div>
+
+const RgbString = ({ rgb }) =>
+  `rgb(${rgb.toString()})`
+
+const getRgbStyle = (rgb) =>
+  `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+
+const fromClassifierToRgb = (classifier) =>
+  classifier[0] > classifier[1]
+    ? [ 255, 255, 255 ]
+    : [ 0, 0, 0 ]
+
+export default App;
